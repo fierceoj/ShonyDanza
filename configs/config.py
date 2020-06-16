33182378c0e0e939564bcd839c16b585a2408630
fierceoj <61847904+fierceoj@users.noreply.github.com>
2020-06-16 17:41:54 -0500

    Update config.py

--- configs/config.py
+++ configs/config.py
@@ -1,20 +1,37 @@
-#configs for shonydanza searches
+#config file for shonydanza searches
 
+#REQUIRED
 #shodan account API key
-#required
+
 API_KEY = ''
 
-#IP or cidr range constraint for searches that return list of IP addresses
-#optional 
-#net_range = '0.0.0.0/0'
 
+#REQUIRED
 #maximum number of results that will be returned per search
-#required
 #default is 100
-search_limit = 100
 
-#honeyscore probability limit
-#IPs exceeding the honeyscore limit will not show up in search results 
-#required
-#change to 1.0 to include all results
-honeyscore_limit = 0.5
+SEARCH_LIMIT = 100
+
+
+#REQUIRED
+#IPs exceeding the honeyscore limit will not show up in IP results
+#adjust to desired probability to adjust results, or change to 1.0 to include all results
+
+HONEYSCORE_LIMIT = 0.5
+
+
+#REQUIRED - at least one key: value pair
+#stock searches that can be selected from a menu
+#add search to the dictionary to automatically add it to your shonydanza menu
+
+STOCK_SEARCHES = {
+'ANONYMOUS_FTP':'ftp anonymous ok',
+'RDP':'port:3389 has_screenshot:true'
+}
+
+
+#OPTIONAL
+#IP or cidr range constraint for searches that return list of IP addresses
+#use comma-separated list to designate multiple (e.g. 1.1.1.1,2.2.0.0/16,3.3.3.3,3.3.3.4) 
+
+#NET_RANGE = '0.0.0.0/0'
