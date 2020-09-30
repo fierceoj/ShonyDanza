6f0661ed37b08a911805f58e53e5f4518fc59832
fierceoj <61847904+fierceoj@users.noreply.github.com>
2020-09-30 09:13:39 -0500

    Added a few more stock searches

--- configs/config.py
+++ configs/config.py
@@ -16,12 +16,24 @@ HONEYSCORE_LIMIT = 1.0
 
 
 #REQUIRED - at least one key: value pair
-#stock searches that can be selected from a menu
-#add search to the dictionary below to add it to your shonydanza stock searches menu
+#add a Shodan dork to the dictionary below to add it to your shonydanza stock searches menu
+#see https://github.com/jakejarvis/awesome-shodan-queries for a great source of queries
+#check into "vuln:" filter if you have Shodan Small Business Plan or higher (e.g., vuln:cve-2019-11510)
 
 STOCK_SEARCHES = {
 'ANONYMOUS_FTP':'ftp anonymous ok',
-'RDP':'port:3389 has_screenshot:true'
+'RDP':'port:3389 has_screenshot:true',
+'OPEN_TELNET':'port:23 console gateway -password',
+'APACHE_DIR_LIST':'http.title:"Index of /"',
+'SPRING_BOOT':'http.favicon.hash:116323821',
+'HP_PRINTERS':'"Serial Number:" "Built:" "Server: HP HTTP"',
+'DOCKER_API':'"Docker Containers:" port:2375',
+'ANDROID_ROOT_BRIDGE':'"Android Debug Bridge" "Device" port:5555',
+'MONGO_EXPRESS_GUI':'"Set-Cookie: mongo-express=" "200 OK"',
+'CVE-2019-11510_PULSE_VPN':'http.html:/dana-na/',
+'CVE-2019-19781_CITRIX_NETSCALER':'http.waf:"Citrix NetScaler"',
+'CVE-2020-5902_F5_BIGIP':'http.favicon.hash:-335242539 "3992"',
+'CVE-2020-3452_CISCO_ASA_FTD':'200 "Set-Cookie: webvpn;"'
 }
 
 
